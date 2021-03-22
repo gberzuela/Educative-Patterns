e9c36bae25250a5dc4732265e2e4a83c4ea3004a
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-03-21 22:47:29 -0700

    Two Pointer: Remove Duplicates

---
+++ Two Pointer/removeDuplicates.js
@@ -0,0 +1,42 @@
+/*
+  Given an array of sorted numbers, remove all duplicates from it. You should NOT USE ANY EXTRA SPACE; after removing the duplicates in place, return the length of the subarry that has no duplicates in it.
+ */
+
+/**
+ * Inputs
+ *
+ * @param arr - sorted array of integers
+ * @returns length of subarray with no duplicates
+ */
+
+/**
+ * Complexity
+ *
+ * Time:  O()
+ * Space: O()
+ */
+
+const removeDuplicates = (arr) => {
+	let next = 1;
+
+	let index = 1;
+	while (index < arr.length) {
+		if (arr[index - 1] !== arr[index]) {
+			arr[next] = arr[index];
+			next++;
+		}
+		index++;
+	}
+
+	return next;
+};
+
+// Example 1
+let arr = [2, 3, 3, 3, 6, 9, 9];
+let result = removeDuplicates(arr);
+console.log(`Input: arr = [${arr}]\nResult: ${result}\nExpected: 4`);
+
+// Example 2
+arr = [2, 2, 2, 11];
+result = removeDuplicates(arr);
+console.log(`Input: arr = [${arr}]\nResult: ${result}\nExpected: 2`);
